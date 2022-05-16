b76e9ec71c692a6f6661a304445390e8aaf0fa98
niboserver <105283598+niboserver@users.noreply.github.com>
2022-05-16 17:38:29 +0800

    upload new version
    upload new versionupload new versionupload new version

---
+++ PlayFabGettingStarted.js
@@ -0,0 +1,28 @@
+function DoExampleLoginWithCustomID(){
+    PlayFab.settings.titleId = "164F7";
+    var loginRequest = {
+        // Currently, you need to look up the required and optional keys for this object in the API reference for LoginWithCustomID. See the Request Headers and Request Body.
+        TitleId: PlayFab.settings.titleId,
+        //CustomId: document.getElementById("customId").value,
+        CustomId:"654654",
+        CreateAccount: true
+    };
+
+    PlayFabClientSDK.LoginWithCustomID(loginRequest, LoginCallback);
+}
+
+// callback functions take two parameters: result and error
+// see callback functions in JavaScript if unclear
+var LoginCallback = function (result, error) {
+
+    if (result !== null) {
+        console.log(result.code);
+        //document.getElementById("resultOutput").innerHTML = "Congratulations, you made your first successful API call!";
+    } else if (error !== null) {
+        // document.getElementById("resultOutput").innerHTML =
+        //     "Something went wrong with your first API call.\n" +
+        //     "Here's some debug information:\n" +
+            PlayFab.GenerateErrorReport(error);
+            console.log('user not found');
+    } 
+}
